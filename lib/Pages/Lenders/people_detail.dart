935abe92e8b49600d686697ee35345e0318b827d
Kendi J <kendi_j@kendis-mbp.lan>
2021-08-17 19:22:47 +0300

    Added firebase collections

--- lib/Pages/Lenders/people_detail.dart
+++ lib/Pages/Lenders/people_detail.dart
@@ -1,10 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class PeopleDetails extends StatefulWidget {
-  const PeopleDetails();
+  final String name;
+  final int number;
+  final String userId;
+
+  const PeopleDetails({this.name, this.number, this.userId});
 
   @override
   _PeopleDetailsState createState() => _PeopleDetailsState();
@@ -12,6 +17,35 @@ class PeopleDetails extends StatefulWidget {
 
 class _PeopleDetailsState extends State<PeopleDetails> {
   final _formKey = GlobalKey<FormBuilderState>();
+
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController itemName = TextEditingController();
+  TextEditingController itemPrice = TextEditingController();
+
+// Added a collection for users on Firestore.
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  List items = [];
+
+  // this function adds a new user to the users collection
+  Future<void> addUser() {
+    return users
+        .add({
+          'name': nameController.text,
+          'phone': phoneController.text,
+          "is_paid": false,
+          "items": items,
+        })
+        .then((value) => print('User added'))
+        .catchError((e) => print("Failed to add user"));
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,79 +62,155 @@ class _PeopleDetailsState extends State<PeopleDetails> {
                   padding: const EdgeInsets.all(8.0),
                   child: FormBuilderTextField(
                     name: 'Name',
+                    controller: nameController,
                     decoration: InputDecoration(
                       labelText: 'Enter a Name',
                     ),
                   ),
                 ),
-                SizedBox(height: 20,),
+                SizedBox(
+                  height: 20,
+                ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: FormBuilderTextField(
                     name: 'Phone',
+                    controller: phoneController,
                     decoration: InputDecoration(
                       labelText: 'Enter phone number',
                     ),
                   ),
                 ),
-                SizedBox(height: 20,),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.ac_unit),
-                    title: Text('mingi'),
-                    trailing: Text("100"),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FloatingActionButton(
+                        child: Icon(Icons.add),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Add items'),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      FormBuilderTextField(
+                                        name: 'Name',
+                                        controller: itemName,
+                                        decoration: InputDecoration(
+                                          labelText: 'Item name',
+                                        ),
+                                      ),
+                                      FormBuilderTextField(
+                                        name: 'Price',
+                                        controller: itemPrice,
+                                        decoration: InputDecoration(
+                                          labelText: 'Item Price',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  actions: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text("Cancel"),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              items.add({
+                                                "name": itemName.text,
+                                                "price": itemPrice.text
+
+                                              });
+                                            });
+                                            itemName.clear();
+                                            itemPrice.clear();
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text("Ok"),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                );
+                              });
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 20,),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.ac_unit),
-                    title: Text('salt'),
-                    trailing: Text("30"),
-                  ),
-                ),
-                SizedBox(height: 20,),
-                ListTile(
-                  leading: Icon(Icons.ac_unit),
-                  title: Text('items'),
-                  trailing: Text("Price"),
-                ),
-                SizedBox(height: 20,),
-                ListTile(
-                  leading: Icon(Icons.ac_unit),
-                  title: Text('items'),
-                  trailing: Text("Price"),
+                SizedBox(
+                  height: 20,
                 ),
               ],
             ),
           ),
-          SizedBox(height: 20,),
-          Row(
-            children: [
-              Expanded(
-                child: MaterialButton(
-                  color: Colors.green,
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white),
+          SizedBox(
+            height: 20,
+          ),
+          ...List.generate(
+           items.length,
+            (index) =>ListTile(
+              leading: Text(items[index]['name']),
+              trailing: Text(items[index]['price']),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CupertinoButton(
+                    color: Colors.green,
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      if (nameController.value.text.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Name cannot be empty')));
+                        return;
+                      }
+                      if (phoneController.value.text.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Number cannot be empty')));
+                        return;
+                      }
+
+                      addUser();
+                    },
                   ),
-                  onPressed: () {},
                 ),
-              ),
-              SizedBox(width: 20),
-              Expanded(
-                child: MaterialButton(
-                  color: Colors.red,
-                  child: Text(
-                    "Reset",
-                    style: TextStyle(color: Colors.white),
+                SizedBox(width: 20),
+                Expanded(
+                  child: CupertinoButton(
+                    color: Colors.red,
+                    child: Text(
+                      "Reset",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _formKey.currentState.reset();
+                    },
                   ),
-                  onPressed: () {
-                    _formKey.currentState.reset();
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,98 +1,120 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:my_tasks/Pages/Lenders/lendees_info.dart';
 import 'package:my_tasks/Pages/Lenders/people_detail.dart';
 
 class MyProfile extends StatefulWidget {
+  final String userId;
+
+  const MyProfile({Key key, this.userId}) : super(key: key);
   @override
   _MyProfileState createState() => _MyProfileState();
 }
 
 class _MyProfileState extends State<MyProfile> {
-
-
-@override
-  void didChangeDependencies() async {
-    super.didChangeDependencies();
-    QuerySnapshot myuser = await FirebaseFirestore.instance
-    .collection('test').get();
-    print(myuser.docs[0].data());
-    
-  }
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Who Owes me'),
-      ),
-      body: Container(
-        child: ListView(
+        appBar: AppBar(
+          title: Text('Who Owes me'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PeopleDetails()),
+            );
+          },
+        ),
+        body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                trailing: FloatingActionButton(
-                  child: Icon(Icons.add),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PeopleDetails()));
-                  },
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Please click on the button below to a new user',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
               ),
             ),
-            ListTile(
-              title: Text('Name'),
-              trailing: Text('Amount'),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'When a user pays, click on their name to get the green tick.',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Card(
-              child: ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TheirInfo()),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _usersStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("Loading");
+                  }
+
+                  return ListView(
+                    children:
+                        snapshot.data.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data() as Map<String, dynamic>;
+                      final total = (data['items'] as List)
+                          .map((e) => double.parse(e['price'] ?? '0'))
+                          .reduce((v, e) => v + e);
+
+                      print(total);
+                      return ListTile(
+                        // updating users data
+                        onTap: () async {
+                          await FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(document.id)
+                              .update({'is_paid': true})
+                              .then((value) => print("User paid"))
+                              .catchError((error) =>
+                                  print("Failed to update user: $error"));
+                        },
+                        title: Text(data['name']),
+                        subtitle: Text(data['phone']),
+                        leading: (data['is_paid'] as bool)
+                            ? Icon(
+                                Icons.check,
+                                color: Colors.green,
+                              )
+                            : Icon(
+                                Icons.close,
+                                color: Colors.red,
+                              ),
+                        trailing: Text("$total"),
+                        // trailing: Text(data['items']),
+                      );
+                    }).toList(),
                   );
                 },
-                title: Text('Mama Bria'),
-                trailing: Text('570/-'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                onTap: () {},
-                title: Text('Julie'),
-                trailing: Text('230/-'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                onTap: () {},
-                title: Text('Katana tim'),
-                trailing: Text('170/-'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                onTap: () {},
-                title: Text('Wa Njoki'),
-                trailing: Text('720/-'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                onTap: () {},
-                title: Text('Silo Silo'),
-                trailing: Text('145/-'),
               ),
             ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
